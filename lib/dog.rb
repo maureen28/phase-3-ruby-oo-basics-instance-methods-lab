614211df05d1d950209332d628951706423c5dec
maureen <maureen.murimi@moringaschool.com>
2022-07-16 19:05:20 +0300

    Add person and dog class

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+dog1 = Dog.new
+dog1.bark
+dog1.sit
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+stacy = Person.new
+stacy.talk
+stacy.walk
